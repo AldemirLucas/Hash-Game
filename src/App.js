f8ae924d89092ff532ff389319d523f9dbeb37f0
Aldemir <aldemirlucas23@gmail.com>
2022-01-29 19:38:38 -0300

    themas color change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import cn from 'classnames'
 import Menu from './components/menu'
 import Game from './components/game'
@@ -9,17 +9,28 @@ function App() {
   const [isLightTheme, setIsLightTheme] = useState(false)
   const [gameState, setGameState] = useState('menu')
   const [players, setPlayers] = useState({})
-
+  
+  useEffect(() => {
+    const theme = localStorage.getItem('theme') === 'dark'
+    setIsLightTheme(theme)
+  },[])
+  
+  const Theme = () => {
+    setIsLightTheme(!isLightTheme)
+    if (isLightTheme) localStorage.setItem('theme','light')
+    else localStorage.setItem('theme','dark')
+  }
+  
   const handlePlay = (players) => {
     setGameState('playing')
     setPlayers(players)
   }
-
+  
   return (
     <div className={cn({"App light" : !isLightTheme}, {"App dark" : isLightTheme})}>
       <ThemeToggle 
         theme= {isLightTheme}
-        onChange= {setIsLightTheme}
+        onChange= {Theme}
       />
       {gameState === 'menu' && <Menu onPlay={handlePlay} />}
       {gameState === 'playing' && 
